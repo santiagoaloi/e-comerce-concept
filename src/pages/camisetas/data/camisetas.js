9928998ea96d8ae75b1d02eef21cafbf427c8a20
Santiago Aloi <santiagoaloi@gmail.com>
2023-05-03 01:27:54 +0200

    chore(camisetas): agregar camisetas

---
+++ src/pages/camisetas/data/camisetas.js
@@ -0,0 +1,90 @@
+export const camisetas = ref([
+  {
+    title: 'Guerrera de la hostia maracana 3x3 con olor a pata',
+    description:
+      'Description 1 Description 1 Description 1Description 1 Description 1 Description 1 Description 1',
+    avatar:
+      'https://images.unsplash.com/photo-1633114128174-2f8aa49759b0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80',
+    rating: '2',
+    price: '10',
+    discount: '4',
+    discountPrice: '8'
+  },
+  {
+    title: 'Guerrera de la hostia  a pata',
+    description:
+      'Description 1 Description 1 Description 1Description 1 Description 1 Description 1 Description 1',
+    avatar:
+      'https://images.unsplash.com/photo-1633114128174-2f8aa49759b0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80',
+    rating: '4',
+    price: '20',
+    discount: '3',
+    discountPrice: '15'
+  },
+  {
+    title: 'Guerrera de la hostia  a patsa',
+    description:
+      'Description 1 Description 1 Description 1Description 1 Description 1 Description 1 Description 1',
+    avatar:
+      'https://images.unsplash.com/photo-1633114128174-2f8aa49759b0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80',
+    rating: '5',
+    price: '30',
+    discount: '2',
+    discountPrice: '25'
+  },
+  {
+    title: 'Guerrera de la hostia  a patsa',
+    description:
+      'Description 1 Description 1 Description 1Description 1 Description 1 Description 1 Description 1',
+    avatar:
+      'https://images.unsplash.com/photo-1633114128174-2f8aa49759b0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80',
+    rating: '5',
+    price: '30',
+    discount: '2',
+    discountPrice: '25'
+  },
+  {
+    title: 'Guerrera de la hostia  a patsa',
+    description:
+      'Description 1 Description 1 Description 1Description 1 Description 1 Description 1 Description 1',
+    avatar:
+      'https://images.unsplash.com/photo-1633114128174-2f8aa49759b0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80',
+    rating: '5',
+    price: '30',
+    discount: '2',
+    discountPrice: '25'
+  },
+  {
+    title: 'Guerrera de la hostia  a patsa',
+    description:
+      'Description 1 Description 1 Description 1Description 1 Description 1 Description 1 Description 1',
+    avatar:
+      'https://images.unsplash.com/photo-1633114128174-2f8aa49759b0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80',
+    rating: '5',
+    price: '30',
+    discount: '2',
+    discountPrice: '25'
+  },
+  {
+    title: 'Guerrera de la hostia  a patsa',
+    description:
+      'Description 1 Description 1 Description 1Description 1 Description 1 Description 1 Description 1',
+    avatar:
+      'https://images.unsplash.com/photo-1633114128174-2f8aa49759b0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80',
+    rating: '5',
+    price: '30',
+    discount: '2',
+    discountPrice: '25'
+  },
+  {
+    title: 'Guerrera de la hostia  a patsa',
+    description:
+      'Description 1 Description 1 Description 1Description 1 Description 1 Description 1 Description 1',
+    avatar:
+      'https://images.unsplash.com/photo-1633114128174-2f8aa49759b0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80',
+    rating: '5',
+    price: '30',
+    discount: '2',
+    discountPrice: '25'
+  }
+])
