6071b843391bd6045bbfad9667f2d1a5420951fd
Santiago Aloi <santiagoaloi@gmail.com>
2023-05-03 01:28:02 +0200

    chore(camisetas): agregar componente

---
+++ src/pages/camisetas/Camisetas.vue
@@ -0,0 +1,21 @@
+<template>
+  <VContainer>
+    <div class="grid gap-6 sm:grid-cols-2 lg:grid-cols-3">
+      <ProductCard
+        v-for="camiseta in camisetas"
+        :key="camiseta.title"
+        :avatar="camiseta.avatar"
+        :description="camiseta.description"
+        :discount="camiseta.discount"
+        :discount-price="camiseta.discountPrice"
+        :price="camiseta.price"
+        :rating="camiseta.rating"
+        :title="camiseta.title"
+      />
+    </div>
+  </VContainer>
+</template>
+
+<script setup>
+import { camisetas } from './data/camisetas'
+</script>
