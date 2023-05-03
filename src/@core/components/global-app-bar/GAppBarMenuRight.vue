fb434c70edde99392d72f9f8560582898a17636b
Santiago Aloi <santiagoaloi@gmail.com>
2023-05-03 22:28:57 +0200

    style(gappbarmenuright): add gap

--- src/@core/components/global-app-bar/GAppBarMenuRight.vue
+++ src/@core/components/global-app-bar/GAppBarMenuRight.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex items-center fixed right-0 mr-5">
+  <div class="flex items-center fixed right-0 mr-5 gap-4">
     <GAppBarThemeSwitch />
     <GappBarFavoriteCounter />
     <GappBarAddedCounter />
