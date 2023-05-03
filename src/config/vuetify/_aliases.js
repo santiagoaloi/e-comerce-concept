12cebfcb48f4913a9d212f0601aaf052be0faa12
Santiago Aloi <santiagoaloi@gmail.com>
2023-05-03 09:53:08 +0200

    chore(aliases): added new aliases

---
+++ src/config/vuetify/_aliases.js
@@ -0,0 +1,9 @@
+import { VBtn } from 'vuetify/components/VBtn'
+import { VRating } from 'vuetify/components/VRating'
+import { VCard } from 'vuetify/components/VCard'
+
+export default {
+  ProductCardActions: VBtn,
+  ProductCardRating: VRating,
+  ProductCard: VCard
+}
