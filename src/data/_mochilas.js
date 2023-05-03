8969b9d3ed821052abf1dbbd511fdcd6f9b4c8e4
Santiago Aloi <santiagoaloi@gmail.com>
2023-05-03 09:51:55 +0200

    chore(mochilas): added product array

---
+++ src/data/_mochilas.js
@@ -0,0 +1,12 @@
+export default [
+  {
+    title: 'Mochila supercomfort para machos de verdad.',
+    description:
+      'Botas Plataforma Cuero Vacuno Mujer Cierre Metalico 751ne Metalico 751ne Metalico 751s',
+    avatar: '/mochilas/mochila1.webp',
+    rating: '2',
+    price: '15.590',
+    discount: '20',
+    discountPrice: '19.490'
+  }
+]
