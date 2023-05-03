7ad2c22fde857490cb6be53b3b6f3b23b6a33a35
Santiago Aloi <santiagoaloi@gmail.com>
2023-05-03 13:21:51 +0200

    chore(favoriteCounter): add component

---
+++ src/@core/components/global-app-bar/GappBarFavoriteCounter.vue
@@ -0,0 +1,11 @@
+<template>
+  <VBtn color="secondary" icon size="small">
+    <VBadge :content="favoriteProductsCounter" color="primary">
+      <VIcon icon="$mdiHeart" />
+    </VBadge>
+  </VBtn>
+</template>
+
+<script setup>
+const { favoriteProductsCounter } = toRefs(useProductStore())
+</script>
