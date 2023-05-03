c5d301ebe52493a562cc73d8c788d51190a224ad
Santiago Aloi <santiagoaloi@gmail.com>
2023-05-03 09:51:50 +0200

    chore(zapatillas): added product array

---
+++ src/data/_zapatos.js
@@ -0,0 +1,32 @@
+export default [
+  {
+    title: 'Botas Botitas Mujer Cuero Base Plataforma Confort Moda 750ne comfort',
+    description:
+      'Botas Plataforma Cuero Vacuno Mujer Cierre Metalico 751ne Metalico 751ne Metalico 751s',
+    avatar: '/zapatos/z2.webp',
+    rating: '2',
+    price: '15.590',
+    discount: '20',
+    discountPrice: '19.490'
+  },
+  {
+    title: 'NIKE',
+    description:
+      'Zapatillas Nike Air Presto Prm 0566 Nike termodinamica con bolsa de aire y sensores de movimiento',
+    avatar: '/zapatos/zap1.webp',
+    rating: '4',
+    price: '61.998',
+    discount: '20',
+    discountPrice: '60.190'
+  },
+  {
+    title: 'NIKE',
+    description:
+      'Zapatillas Nike Air Presto Prm 0566 Nike termodinamica con bolsa de aire y sensores de movimiento',
+    avatar: '/zapatos/zap2.webp',
+    rating: '4',
+    price: '69.928',
+    discount: '10',
+    discountPrice: '19.490'
+  }
+]
