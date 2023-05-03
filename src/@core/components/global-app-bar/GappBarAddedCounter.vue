71b5438b1e26ec51e902875416385f49bc2c1b12
Santiago Aloi <santiagoaloi@gmail.com>
2023-05-03 13:22:01 +0200

    chore(addedCounter): add component

---
+++ src/@core/components/global-app-bar/GappBarAddedCounter.vue
@@ -0,0 +1,11 @@
+<template>
+  <VBtn color="secondary" icon size="small">
+    <VBadge :content="addedProductsCounter" color="primary">
+      <VIcon icon="$mdiStoreOutline" />
+    </VBadge>
+  </VBtn>
+</template>
+
+<script setup>
+const { addedProductsCounter } = toRefs(useProductStore())
+</script>
