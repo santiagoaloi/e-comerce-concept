16c7a4161840ddcb906fb4848863dd77e116d67c
Santiago Aloi <santiagoaloi@gmail.com>
2023-05-03 13:21:30 +0200

    feat(counters): aded appbar counters

--- src/@core/components/global-app-bar/GAppBarMenuRight.vue
+++ src/@core/components/global-app-bar/GAppBarMenuRight.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="flex items-center fixed right-0 mr-5">
     <GAppBarThemeSwitch />
-    <GappBarStoreCounter />
+    <GappBarFavoriteCounter />
+    <GappBarAddedCounter />
   </div>
 </template>
