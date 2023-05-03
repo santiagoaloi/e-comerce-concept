f043b2fc827b2383795ec8f8524572c3e7078515
Santiago Aloi <santiagoaloi@gmail.com>
2023-05-03 13:19:43 +0200

    refactor(productStore): persistance fix

--- src/stores/productStore.js
+++ src/stores/productStore.js
@@ -2,30 +2,63 @@ import { mochilas, zapatos } from '@/data'
 
 export const useProductStore = defineStore('global-products', {
   state: () => ({
-    products: [...mochilas, ...zapatos].map((product) => {
-      const _cart = {
-        added: 0,
-        favorited: false
-      }
-      return { ...product, _cart }
-    })
+    products: []
   }),
 
-  // persist: true,
+  persist: true,
 
   getters: {
-    cartProducts() {
+    addedProducts() {
       return this.products.filter((product) => {
         return product._cart.added
       })
     },
 
-    cartProducsCounter() {
-      return this.cartProducts.reduce((acc, prod) => (acc += prod._cart.added), 0)
+    favoriteProducts() {
+      return this.products.filter((product) => {
+        return product._cart.favorite
+      })
+    },
+
+    addedProductsCounter() {
+      // sum the number of units added across all selected products.
+      return this.addedProducts.reduce((acc, prod) => (acc += prod._cart.added), 0)
+    },
+
+    favoriteProductsCounter() {
+      // sum the number of units added across all selected products.
+      return this.favoriteProducts.reduce((acc, prod) => (acc += Number(prod._cart.favorite)), 0)
     }
   },
+  actions: {
+    simulateProductsGet() {
+      // Fetch newProducts from backend.
+      const newProducts = [...mochilas, ...zapatos].map((product) => {
+        // Add the _cart object to every product object.
+        const _cart = {
+          added: 0,
+          favorite: false
+        }
+        return { ...product, _cart }
+      })
 
-  actions: {}
+      const mergedProducts = newProducts.map((product) => {
+        // find the corresponding object in newProducts based on product id.
+        const matchingProduct = this.products.find((item) => item._id === product._id)
+
+        // merge _cart values into the resulting object.
+        if (matchingProduct) {
+          return { ...product, _cart: matchingProduct._cart }
+        }
+
+        // if there is no matching product,
+        // just return the original product object from newProducts.
+        return product
+      })
+
+      this.products = mergedProducts
+    }
+  }
 })
 
 if (import.meta.hot) import.meta.hot.accept(acceptHMRUpdate(useProductStore, import.meta.hot))
