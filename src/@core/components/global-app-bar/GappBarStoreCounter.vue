68c58b770c7d6273d9ad8e02f54283f28c72327a
Santiago Aloi <santiagoaloi@gmail.com>
2023-05-03 09:53:39 +0200

    feat(appbar): cart icon counter

---
+++ src/@core/components/global-app-bar/GappBarStoreCounter.vue
@@ -0,0 +1,11 @@
+<template>
+  <VBtn color="secondary" icon size="small">
+    <VBadge :content="cartProducsCounter" color="error">
+      <VIcon icon="$mdiStoreOutline" />
+    </VBadge>
+  </VBtn>
+</template>
+
+<script setup>
+const { cartProducsCounter } = toRefs(useProductStore())
+</script>
