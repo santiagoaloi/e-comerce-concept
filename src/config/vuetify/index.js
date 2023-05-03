57699dd0f3069726d5f8957b6f03404329d5f89b
Santiago Aloi <santiagoaloi@gmail.com>
2023-05-03 23:48:04 +0200

    chore(defaults): segregated vuetify

--- src/config/vuetify/index.js
+++ src/config/vuetify/index.js
@@ -1,6 +1,7 @@
 import theme from './_themes'
 import aliases from './_aliases'
 import * as icons from './_icons'
-import defaults from './_defaults'
+import defaultsNative from './_defaultsNative'
+import defaultsCustom from './_defaultsCustom'
 
-export { theme, icons, defaults, aliases }
+export { theme, icons, defaultsNative, defaultsCustom, aliases }
