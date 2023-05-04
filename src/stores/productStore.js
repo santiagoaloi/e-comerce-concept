f3a0da7279fdd0ee269a5519e1756034dd685450
Santiago Aloi <santiagoaloi@gmail.com>
2023-05-04 08:02:17 +0200

    refactor(produtStore): add/remove/paginated logic

--- src/stores/productStore.js
+++ src/stores/productStore.js
@@ -1,22 +1,82 @@
+import NProgress from 'nprogress'
+
 export const useProductStore = defineStore('global-products', {
   state: () => ({
     selectedProducts: [],
-    products: []
+    products: [],
+    cart: [],
+    favorites: [],
+    page: 1,
+    loading: false
   }),
 
-  persist: {
-    paths: ['selectedProducts']
-  },
+  persist: true,
 
   getters: {},
 
   actions: {
-    async getProducts() {
+    findCartProduct(product) {
+      return this.cart.find((cartProduct) => cartProduct.id === product.id)
+    },
+
+    favoriteCartUnit(product) {
+      const productExists = this.cart.find((cartProduct) => cartProduct.id === product.id)
+      // const favoriteExists = this.favorites.find((favoriteProduct) => cartProduct.id === product.id)
+
+      if (productExists) {
+        productExists._cart.favorite = !productExists._cart.favorite
+        return
+      }
+    },
+
+    removeProductFromCart(id) {
+      this.cart = this.cart.filter((product) => {
+        return product.id !== id
+      })
+    },
+
+    decreaseCartUnit(product) {
+      const productExists = this.cart.find((cartProduct) => cartProduct.id === product.id)
+
+      if (productExists && productExists._cart.units > 1) {
+        productExists._cart.units -= 1
+        return
+      }
+
+      if (productExists) this.removeProductFromCart(productExists.id)
+    },
+
+    increaseCartUnit(product) {
+      const productExists = this.cart.find((cartProduct) => cartProduct.id === product.id)
+
+      if (!productExists) {
+        product._cart.units += 1
+        this.cart.push(product)
+        return
+      }
+
+      productExists._cart.units += 1
+    },
+
+    async switchPaginationPage(page) {
       try {
-        const result = await axios.post('/publicProduct.getAll')
-        this.products = result.data.result
+        NProgress.start()
+        this.loading = true
+
+        const result = await axios.post(`/publicProduct.getAll?page=${page}`)
+        const paginatedProducts = result.data.result
+
+        paginatedProducts.data = paginatedProducts.data.map((product) => {
+          return { ...product, _cart: { units: 0, favorite: false } }
+        })
+
+        this.products = paginatedProducts
       } catch (e) {
+        this.loading = false
         console.log(e)
+      } finally {
+        this.loading = false
+        NProgress.done()
       }
     }
   }
