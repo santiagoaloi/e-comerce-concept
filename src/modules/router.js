cbc159d07decacf192e8fbb3b944c738d133a1d5
Santiago Aloi <santiagoaloi@gmail.com>
2023-05-06 07:46:44 +0200

    chore(router): removed progress indicator

--- src/modules/router.js
+++ src/modules/router.js
@@ -3,7 +3,7 @@ import { setupLayouts } from 'virtual:generated-layouts'
 
 // Progress bar on top of the page.
 // Customize styles in @/styles/_progress-bar.css
-import NProgress from 'nprogress'
+// import NProgress from 'nprogress'
 
 const router = createRouter({
   history: createWebHistory(),
@@ -13,13 +13,13 @@ const router = createRouter({
   scrollBehavior: () => ({ left: 0, top: 0 })
 })
 
-router.beforeEach(() => {
-  NProgress.start()
-})
+// router.beforeEach(() => {
+//   NProgress.start()
+// })
 
-router.afterEach(() => {
-  NProgress.done()
-})
+// router.afterEach(() => {
+//   NProgress.done()
+// })
 
 const install = (app) => app.use(router)
 
