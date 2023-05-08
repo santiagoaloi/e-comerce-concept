d670e657427e163d3576caf8c0ea7b699c1a2c50
Santiago Aloi <santiagoaloi@gmail.com>
2023-05-08 09:36:12 +0200

    feat(http-service): created axios eervice

---
+++ src/utils/http-service.js
@@ -0,0 +1,29 @@
+import axios from 'axios'
+
+/** Create an Axios instance. */
+function createService() {
+  return axios.create()
+}
+
+function createRequestFunction(service) {
+  return function (config) {
+    const configDefault = {
+      headers: {
+        /**  Authorization: 'Bearer ' + getToken(), */
+        'Content-Type': 'application/json',
+        'X-Requested-With': 'XMLHttpRequest'
+      },
+      withCredentials: true,
+      timeout: 5000,
+      baseURL: import.meta.env.VITE_BaseApiURL,
+      data: {}
+    }
+    return service({ ...configDefault, ...config })
+  }
+}
+
+/** Instance for network request */
+export const service = createService()
+
+/** Method for network request */
+export const request = createRequestFunction(service)
