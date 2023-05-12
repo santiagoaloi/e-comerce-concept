b39ed285e83b27ffa72597952a5ef06cd6263544
Santiago Aloi <santiagoaloi@gmail.com>
2023-05-12 23:36:37 +0200

    feat: add noProductsFind component

---
+++ src/pages/products/components/NoProductsFound.vue
@@ -0,0 +1,15 @@
+<template>
+  <div
+    v-if="!products?.data?.length && !isLoading"
+    class="d-flex fill-height align-center justify-center"
+  >
+    <NotFound
+      subtitle="Posiblemente la pagina no exista o no haya ningun producto a la venta"
+      title="No hay productos para mostrar"
+      top="ups!"
+    />
+  </div>
+</template>
+<script setup>
+const { isLoading, products } = makeStoreDestructurable(useProductStore())
+</script>
